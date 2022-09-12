759c74fd5b1d6aae24b16bd8ad0b6d70a676cfc6
ShravyaPrakash <70698616+ShravyaPrakash@users.noreply.github.com>
2022-09-12 19:06:36 +0530

    Add files via upload

---
+++ Phase2_Section4_Retrieve student data/Section6/Section6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Section6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = File.ReadAllText("C:\\Users\\HP\\Simplilearn_Practice_Projects\\StudentData.txt");
+            Console.WriteLine(data);
+        }
+    }
+}
