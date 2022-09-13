4150ea88530020e3f0e564be4bfa67da7e1b864a
ShravyaPrakash <70698616+ShravyaPrakash@users.noreply.github.com>
2022-09-13 14:54:13 +0530

    commit

---
+++ Phase2_Section9_oops/Section9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using ClassLibrary1;
+namespace Section9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student s = new student();
+            s.gets();
+            s.prints();
+            s = new teacher();
+            s.gets();
+            s.prints();
+            s = new subject();
+            s.gets();
+            s.prints();
+
+        }
+    }
+}
