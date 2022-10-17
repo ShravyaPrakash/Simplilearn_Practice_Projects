f89754d35db1ef716f80016e659b7730b37c35db
ShravyaPrakash <70698616+ShravyaPrakash@users.noreply.github.com>
2022-10-17 21:10:44 +0530

    commit

---
+++ Phase3_FinalProject_EMS/Models/EmployeeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EMS_Full_Stack.Models
+{
+    public class EmployeeModel
+    {
+        [Required()]
+        [Key]
+        public int EmpCode { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string EmpName { get; set; }
+        public string Email { get; set; }
+        public int DeptCode { get; set; }
+    }
+}
